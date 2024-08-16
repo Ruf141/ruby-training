8b9702f9010239a0d3e5fd6a46e6418409dc5b84
Ruf141 <mad0908141@gmail.com>
2024-08-16 23:09:20 +0900

    commit

--- method.rb
+++ method.rb
@@ -32,3 +32,21 @@ end
 hello()
 hello("Newbie")
 
+puts "return文"
+def volume (x, y, z)
+  return x * y * z
+end
+
+p volume(2, 3, 4)
+p volume(10, 20, 30)
+
+puts "return文は省略可能"
+def area(x, y, z)
+  xy = x * y
+  yz = y * z
+  zx = z * x
+  (xy + yz + zx) * 2
+end
+
+p area(2, 3, 4)
+p area(10, 20, 30)
