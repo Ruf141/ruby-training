f50979026b1f90a3d6f1c5204923209fac9007da
Ruf141 <mad0908141@gmail.com>
2024-08-18 21:14:26 +0900

    commit

---
+++ class.rb
@@ -0,0 +1,22 @@
+=begin
+クラスとはオブジェクトの種類を表すもの
+Rubyでは型と言い換えてよい
+配列→配列クラスのオブジェクト
+
+=end
+
+#オブジェクトがどのクラスに属しているかはclassメソッドを使う
+
+ary = []
+str = "Hello world"
+p ary.class
+p str.class
+
+
+#オブジェクトどのクラスのインスタンスか確認するにはinstance_of?メソッドを使う
+ary = []
+str = "Hello world"
+p ary.instance_of?(Array)
+p str.instance_of?(String)
+p ary.instance_of?(String)
+p str.instance_of?(Array)
